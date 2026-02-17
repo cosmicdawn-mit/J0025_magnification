8053093ce803672a51bb02ffc487743757e3f0be
KMPanebianco <kpanebianco@me.com>
2026-02-17 20:18:50 +0000

    Add files via upload

---
+++ lensingmodel_J0025/F606W/compute_positions.py
@@ -0,0 +1,22 @@
+import os, sys
+import numpy as np
+
+# this is ACS image
+pixscale = 0.05
+
+# galaxy
+xg = 49.8566
+yg = 50.4680
+ar = 0.4733
+pa = 68.8965
+
+# qso
+xq = 37.9065
+yq = 51.0470
+
+# compute the relative positions of the quasar. Set xg, yg = 0, 0
+
+xp = (xq-xg)*pixscale
+yp = (yq-yg)*pixscale
+
+print(xp, yp, ar, pa)
